3903a211c51c83fb65ef8845d0e2f48dbd6af067
gkaln <55789275+gkaln@users.noreply.github.com>
2019-10-23 16:08:17 +0300

    Update mylosses.py

--- util/mylosses.py
+++ util/mylosses.py
@@ -3,7 +3,7 @@ Implementation of the subclass sigmoid cross entropy loss.
 
 History
 -------
-DATE       | DESCRIPTION    | NAME              | Organization |
+DATE       | DESCRIPTION    | NAME              | ORGANIZATION |
 21/07/2019 | first creation | Nikolaos Gkalelis | CERTH-ITI    |
 """
 
